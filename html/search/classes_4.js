ac239210f432fed20875c8ad184629364a389eb6
notyourav <notyourav@users.noreply.github.com>
2022-02-08 21:06:57 +0000

    deploy: 358f8c87cce6e0c84e31222b737508bf2194051f

--- html/search/classes_4.js
+++ html/search/classes_4.js
@@ -2,8 +2,9 @@ var searchData=
 [
   ['eepromconfig_0',['EEPROMConfig',['../structEEPROMConfig.html',1,'']]],
   ['element_1',['Element',['../structElement.html',1,'']]],
-  ['enemy64entity_2',['Enemy64Entity',['../structEnemy64Entity.html',1,'']]],
-  ['enemydefinition_3',['EnemyDefinition',['../structEnemyDefinition.html',1,'']]],
-  ['entity_4',['Entity',['../structEntity.html',1,'']]],
-  ['entitydata_5',['EntityData',['../structEntityData.html',1,'']]]
+  ['enemy50entity_2',['Enemy50Entity',['../structEnemy50Entity.html',1,'']]],
+  ['enemy64entity_3',['Enemy64Entity',['../structEnemy64Entity.html',1,'']]],
+  ['enemydefinition_4',['EnemyDefinition',['../structEnemyDefinition.html',1,'']]],
+  ['entity_5',['Entity',['../structEntity.html',1,'']]],
+  ['entitydata_6',['EntityData',['../structEntityData.html',1,'']]]
 ];
--- html/search/files_4.js
+++ html/search/files_4.js
@@ -7,13 +7,14 @@ var searchData=
   ['enemy_2ec_4',['enemy.c',['../enemy_8c.html',1,'']]],
   ['enemy_2eh_5',['enemy.h',['../enemy_8h.html',1,'']]],
   ['enemy4d_2ec_6',['enemy4D.c',['../enemy4D_8c.html',1,'']]],
-  ['enemy64_2ec_7',['enemy64.c',['../enemy64_8c.html',1,'']]],
-  ['enemy66_2ec_8',['enemy66.c',['../enemy66_8c.html',1,'']]],
-  ['enemye_2ec_9',['enemyE.c',['../enemyE_8c.html',1,'']]],
-  ['enemyutils_2ec_10',['enemyUtils.c',['../enemyUtils_8c.html',1,'']]],
-  ['enterportalsubtask_2ec_11',['enterPortalSubtask.c',['../enterPortalSubtask_8c.html',1,'']]],
-  ['entity_2ec_12',['entity.c',['../entity_8c.html',1,'']]],
-  ['entity_2eh_13',['entity.h',['../entity_8h.html',1,'']]],
-  ['epona_2ec_14',['epona.c',['../epona_8c.html',1,'']]],
-  ['ezlo_2ec_15',['ezlo.c',['../ezlo_8c.html',1,'']]]
+  ['enemy50_2ec_7',['enemy50.c',['../enemy50_8c.html',1,'']]],
+  ['enemy64_2ec_8',['enemy64.c',['../enemy64_8c.html',1,'']]],
+  ['enemy66_2ec_9',['enemy66.c',['../enemy66_8c.html',1,'']]],
+  ['enemye_2ec_10',['enemyE.c',['../enemyE_8c.html',1,'']]],
+  ['enemyutils_2ec_11',['enemyUtils.c',['../enemyUtils_8c.html',1,'']]],
+  ['enterportalsubtask_2ec_12',['enterPortalSubtask.c',['../enterPortalSubtask_8c.html',1,'']]],
+  ['entity_2ec_13',['entity.c',['../entity_8c.html',1,'']]],
+  ['entity_2eh_14',['entity.h',['../entity_8h.html',1,'']]],
+  ['epona_2ec_15',['epona.c',['../epona_8c.html',1,'']]],
+  ['ezlo_2ec_16',['ezlo.c',['../ezlo_8c.html',1,'']]]
 ];
