058acb6ce3e9fb231759143bcc87627b991b4a8c
notyourav <notyourav@users.noreply.github.com>
2022-03-21 02:29:58 +0000

    deploy: f57f815879ca5d743557125341bf583b48bbdeaf

--- html/search/classes_10.js
+++ html/search/classes_10.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['rcnt_0',['RCnt',['../structRCnt.html',1,'']]],
   ['roomcontrols_1',['RoomControls',['../structRoomControls.html',1,'']]],
-  ['roomresinfo_2',['RoomResInfo',['../structRoomResInfo.html',1,'']]],
-  ['roomtransition_3',['RoomTransition',['../structRoomTransition.html',1,'']]],
-  ['roomvars_4',['RoomVars',['../structRoomVars.html',1,'']]]
+  ['roommemory_2',['RoomMemory',['../structRoomMemory.html',1,'']]],
+  ['roomresinfo_3',['RoomResInfo',['../structRoomResInfo.html',1,'']]],
+  ['roomtransition_4',['RoomTransition',['../structRoomTransition.html',1,'']]],
+  ['roomvars_5',['RoomVars',['../structRoomVars.html',1,'']]]
 ];
